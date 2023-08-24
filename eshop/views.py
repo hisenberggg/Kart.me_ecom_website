15173b492e3d423b9ef512d1f941932857035444
Abhijeet Ringe <65344317+hisenberggg@users.noreply.github.com>
2023-08-24 12:39:08 +0530

    Update views.py

--- eshop/views.py
+++ eshop/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url="/auth/login/")
 def home(request):
     products = Product.objects.all()
-    print(products[0].product_image)
+    # print(products[0].product_image)
     context = {
         "products":products
     }
